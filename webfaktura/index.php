44c31af01445c692d849210e4d917afc7e58ce85
hsnx_krsnx <hsnx_krsnx>
2002-09-17 17:25:08 +0000

    * Erstes Modul für Kundenverwaltung portieren

--- webfaktura/index.php
+++ webfaktura/index.php
@@ -8,7 +8,14 @@
 # initializiere alles
 include("lib/init.php");
 
+$page = new page();
+$page->init("WebFaktura");
 
+$mod_name="mod_$action";
+$mod = new $action($page, $db);
+$mod->start();
+$page->render();
+$page->output();
 
 
 ?>
