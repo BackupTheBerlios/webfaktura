f83f04cc63f5407d4387489784312da90a542c49
hsnx_krsnx <hsnx_krsnx>
2002-10-02 19:29:38 +0000

    * Bugfixes for PHP 4.2 * Notice that this application is Alpha and lacks a lot of funtionality!

--- webfaktura/index.php
+++ webfaktura/index.php
@@ -11,11 +11,11 @@ include("lib/init.php");
 $page = new page();
 $page->init("WebFaktura");
 $page->generate_menu();
-$mod = new $action($page, $db);
+$mod = new $action(&$page, &$db);
 $mod->start();
 $mod->render();
 $page->render();
 $page->output();
-
+echo $action;
 
 ?>
