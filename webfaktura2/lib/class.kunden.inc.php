69a2b3409ddd1b0fab6dfbee3c7e2fd0aed3f5d0
hsnx_krsnx <hsnx_krsnx>
2003-03-04 18:34:30 +0000

    *** empty log message ***

--- webfaktura2/lib/class.kunden.inc.php
+++ webfaktura2/lib/class.kunden.inc.php
@@ -125,7 +125,7 @@ class kunden extends page{
 		$pdf->Ln(10);
 		$pdf->SetFont("Arial", "", 10);
 		$pdf->Write(5, "Sehr geehrte Damen und Herren,\nhiermit erlaube ich mir folgendes in Rechnung zu stellen:");
-		$query="select posten.datum as Datum, posten.kommentar as Beschreibung, posten.anzahl as Anzahl, produkte.name as Artikel, produkte.preis as Preis, (produkte.preis*posten.anzahl) as Summe from posten, produkte where posten.rechnung='$id' and $produkte.id=$posten.produkt";
+		$query="select posten.datum as Datum, posten.kommentar as Beschreibung, posten.anzahl as Anzahl, produkte.name as Artikel, produkte.preis as Preis, (produkte.preis*posten.anzahl) as Summe from posten, produkte where posten.rechnung='$id' and produkte.id=posten.produkt";
 		$result=$db->query($query);
 		$header=array("Datum", "Beschreibung", "Anzahl", "Artikel", "Preis", "Summe");
 		while($data[]=$db->get_row($result))
--- webfaktura2/lib/class.pdf.inc.php
+++ webfaktura2/lib/class.pdf.inc.php
@@ -73,6 +73,27 @@ class pdf extends FPDF
 		//Page number
 		$this->Cell(0,10,$GLOBALS["conf"]["rechnung"]["bankverbindung"],0,0,'C');
 	}
+	
+	function table($header, $data){
+		//Column widths
+    		$w=array(20,60,10,30,15,15);
+    		//Header
+    		for($i=0;$i<count($header);$i++)
+        		$this->Cell($w[$i],7,$header[$i],1,0,'C');
+    		$this->Ln();
+    		//Data
+    		foreach($data as $row)
+    		{
+        		$this->Cell($w[0],6,$row[0],'LR');
+        		$this->Cell($w[1],6,$row[1],'LR');
+			$this->Cell($w[2],6,number_format($row[2]),'LR',0,'R');
+        		$this->Cell($w[3],6,$row[3],'LR');
+			$this->Cell($w[4],6,number_format($row[4])." ¤", 'LR');
+			$this->Cell($w[5],6,number_format($row[5])." ¤", 'LR');
+        		$this->Ln();
+    		}
+    		//Closure line
+    		$this->Cell(array_sum($w),0,'','T');
+	}
 }
-
 ?>
