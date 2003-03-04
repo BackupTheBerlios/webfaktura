17bc5878db322b608d3d319c49e327de156e239e
hsnx_krsnx <hsnx_krsnx>
2003-03-04 18:45:20 +0000

    *** empty log message ***

--- webfaktura2/lib/class.pdf.inc.php
+++ webfaktura2/lib/class.pdf.inc.php
@@ -76,7 +76,7 @@ class pdf extends FPDF
 	
 	function table($header, $data){
 		//Column widths
-    		$w=array(20,60,15,40,15,15);
+    		$w=array(20,56,15,43,15,15);
     		//Header
     		for($i=0;$i<count($header);$i++)
         		$this->Cell($w[$i],7,$header[$i],1,0,'C');
@@ -85,11 +85,11 @@ class pdf extends FPDF
     		foreach($data as $row)
     		{
         		$this->Cell($w[0],6,$row[0],'LR');
-        		$this->Cell($w[1],6,substr($row[1],0,50),'LR');
+        		$this->Cell($w[1],6,substr($row[1],0,30),'LR');
 			$this->Cell($w[2],6,number_format($row[2],2,",","."),'LR',0,'R');
         		$this->Cell($w[3],6,$row[3],'LR');
-			$this->Cell($w[4],6,number_format($row[4],2,",",".")." EURO", 'LR');
-			$this->Cell($w[5],6,number_format($row[5],2,",",".")." EURO", 'LR');
+			$this->Cell($w[4],6,number_format($row[4],2,",",".").EURO, 'LR');
+			$this->Cell($w[5],6,number_format($row[5],2,",",".").EURO, 'LR');
         		$this->Ln();
     		}
     		//Closure line
